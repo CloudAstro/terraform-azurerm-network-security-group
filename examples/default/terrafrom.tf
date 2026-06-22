b51d8e65c869080b75351187c8e7aa88d5a0a757
dibranica <erjon.dibrani@cloudastro.de>
2026-06-22 08:24:46 +0200

    feat: update tf and provider version

--- examples/default/terrafrom.tf
+++ examples/default/terrafrom.tf
@@ -1,3 +1,9 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_version = ">= 1.15"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.77"
+    }
+  }
 }
